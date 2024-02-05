231699b6b1c72260d2d56ca3278e44dc017d88c1
Krittin Chaowakarn <130139478+krittin-ch@users.noreply.github.com>
2024-02-05 15:49:46 +0700

    Upload

---
+++ RandomImage_UnsplashAPI/app.js
@@ -0,0 +1,15 @@
+const container = document.querySelector(".container")
+const rows = 3
+const cols = 3
+
+function randomNumber() {
+    return Math.floor(Math.random()*100)
+}
+
+for (let i=0; i<rows*cols; i++) {
+    const url = `https://source.unsplash.com/random/${randomNumber()}`
+
+    const imageEl = document.createElement("img")
+    imageEl.src = url
+    container.appendChild(imageEl)
+}
