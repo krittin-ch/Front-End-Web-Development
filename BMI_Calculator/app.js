b6d4e1b243b55e4f426003c645e28063d3f0e5b8
Krittin Chaowakarn <130139478+krittin-ch@users.noreply.github.com>
2024-02-06 21:07:16 +0700

    Upload

--- BMI_Calculator/app.js
+++ BMI_Calculator/app.js
@@ -1,1 +1,14 @@
-const weight = document.getElementById("weight")
+const Btn = document.querySelector(".btn")
+
+function CalculateBMI() {
+    let weight = document.getElementById("weight").value
+    let height = document.getElementById("height").value
+    BMI = weight/((height/100)**2)
+    return BMI
+}
+
+function showBMI() {
+    console.log(CalculateBMI())
+}
+
+Btn.addEventListener("click", showBMI)
