62c4ddfa575c1b858915681e43a53938fa8fff8a
Krittin Chaowakarn <130139478+krittin-ch@users.noreply.github.com>
2024-02-08 13:55:33 +0700

    Upload

--- BMI_Calculator/app.js
+++ BMI_Calculator/app.js
@@ -1,4 +1,5 @@
 const Btn = document.querySelector(".btn")
+const result = document.querySelector(".result")
 
 function CalculateBMI() {
     let weight = document.getElementById("weight").value
@@ -7,8 +8,30 @@ function CalculateBMI() {
     return BMI
 }
 
-function showBMI() {
-    console.log(CalculateBMI())
+function showResult(bmi, messege, color) {
+    result.style.backgroundColor = color
+    return result.innerHTML = `Result: BMI = ${bmi} (${messege})`
 }
+
+function showBMI() {
+    if (weight === "" || isNaN(weight)) {
+        return result.innerHTML = "Please Enter Your Weight"
+    } else if (height === "" || isNaN(height)) {
+        return result.innerHTML = "Please Enter Your Height"
+    } else {
+        if (BMI < 18.5) {
+            showResult(BMI, "Underweight", "cyan")
+        } else if (BMI >= 18.5 && BMI < 25) {
+            showResult(BMI, "Normal", "green")
+        } else if (BMI >= 25 && BMI < 30) {
+            showResult(BMI, "Overweight", "yellow")
+        } else if (BMI >= 30 && BMI < 35) {
+            showResult(BMI, "Obese", "orange")
+        } else {
+            showResult(BMI, "Extremly Obese", "red")
+        }
+    }
+}
+
 
 Btn.addEventListener("click", showBMI)
